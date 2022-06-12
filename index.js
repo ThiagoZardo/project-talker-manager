700ce0e3ba0242e43f13599d716b0986268e4f62
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-06-11 23:10:08 -0300

    Requisito 01 ok

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs/promises');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,6 +7,18 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const getTalkers = async (_req, res) => {
+  try {
+    const talkersJSON = JSON.parse(await fs.readFile('./talker.json'));
+    if (talkersJSON.length === 0) return res.status(HTTP_OK_STATUS).send([]);
+    return res.status(HTTP_OK_STATUS).json(talkersJSON);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get('/talker', getTalkers);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
